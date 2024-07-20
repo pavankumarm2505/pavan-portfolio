b12c4d67150223443145d5454ea48cfc85148568
Pavan Kumar Mathamsetty <pavankumarm2505@gmail.com>
2024-07-20 16:05:32 -0400

    third commit-bento grid

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -8,7 +8,7 @@ const MagicButton = ({
   return (
     <button className="p-[3px] relative w-full md:w-60 md:mt-10">
       <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-      <div className="px-8 py-2  bg-black-100 rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-between gap-x-1">
+      <div className="px-8 py-2  bg-black-100 rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-between">
         {position === 'left' && <span className="mr-1">{icon}</span>}
         {title}
         {position === 'right' && <span className="mr-1">{icon}</span>}
